b37c5128d7bb181aed2ea5f5a4d5ee3aab721cd8
EM-OBI <gbassey1@byupathway.edu>
2026-05-06 15:03:19 +0100

    Better calculator with switch statements

--- main.cpp
+++ main.cpp
@@ -3,22 +3,48 @@
 
 using namespace std; 
 
-int getMax (int num1, int num2) {
-    int result;
 
-    if (num1 > num2) {
-        result = num1;
-    } else {
-        result = num2;
-    }
-
-    return result;
-}
 
 int main() {
 
-    cout <<  getMax(129, 98) << endl;
-   
+    int num1, num2;
+    char op;
+    int result;
+
+    cout << "Enter first number: ";
+    cin >> num1; 
+
+    cout << "Enter operator: ";
+    cin >> op; 
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    switch (op)
+    {
+    case '+':
+        result = num1 + num2;
+        break;
+    
+    case '-':
+        result =  num1 - num2;
+        break;
+    
+    case '*':
+        result =  num1 * num2;
+        break;
+    
+    case '/':
+        result =  num1 / num2;
+        break;
+
+    default: 
+        cout << "Please exit program and enter a valid operator!" << endl;
+    }
+    
+
+    cout << result << endl;
+
     return 0;
 }
 
