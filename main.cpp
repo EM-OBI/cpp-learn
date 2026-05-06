2012d46690b1d849981dd85e43a12c338d95af7e
EM-OBI <gbassey1@byupathway.edu>
2026-05-06 10:02:03 +0100

    Learning C++ commencement

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string name = "John";
+    int age;
+    age = 18;
+
+    cout << "His name is " << name << endl;
+    cout << name << " loves to ride bikes" << endl;
+    cout << name << " is " << age << " years old" << endl;
+    cout << "He turned " << age << " years old yesterday" << endl;
+
+    return 0;
+}
