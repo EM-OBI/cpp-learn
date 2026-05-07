22387b835c3faa848ec39f7bb0001b84e09a43f4
EM-OBI <gbassey1@byupathway.edu>
2026-05-07 11:11:44 +0100

    Nested arrays

--- main.cpp
+++ main.cpp
@@ -3,18 +3,20 @@
 
 using namespace std; 
 
-double exponent (double number, int power) {
-    double result = number;
-
-    for (int i = 1; i < power; i++) {
-        result *= number;
-    }
-
-    return result;
-}
-
 int main() {
-    cout << exponent (3, 3) << endl;
+    
+    int numberGrid[3][2] =  {
+        {1, 2},
+        {3, 4},
+        {5, 6}
+    };
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 2; j++) {
+            cout << numberGrid[i][j];
+        }
+        cout << endl;
+    }
 
     return 0;
 }
