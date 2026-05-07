8ab0d685a40f1b7381c4f33c98616711774efbbd
EM-OBI <gbassey1@byupathway.edu>
2026-05-07 09:56:24 +0100

    for loop

--- main.cpp
+++ main.cpp
@@ -6,26 +6,9 @@ using namespace std;
 
 
 int main() {
-    srand(time(0));
-    int upper = 11;
-    int number = rand() % upper;
-    
-    int guess;
-
-    int guessCount = 0;
-    
-    while (guess != number) {
-        cout << "Guess a number between 0 and " << upper - 1 << endl;
-        cin >> guess;
-        guessCount++;
-        if (guess > number) {
-            cout << "Too high, try again!" << endl;
-        } else {
-            cout << "Too low, try again!" << endl;
-        }
+    for (int i = 1; i <= 5; i++) {
+        cout << i << endl;
     }
-    
-    cout << "Congrats! you guessed " << guess << " after " << guessCount << " guesses!" << endl;
 
     return 0;
 }
