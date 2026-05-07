19fffd2aff3559d4ea97c9fefbf35239fa40104b
EM-OBI <gbassey1@byupathway.edu>
2026-05-07 09:24:59 +0100

    While loops

--- main.cpp
+++ main.cpp
@@ -7,43 +7,12 @@ using namespace std;
 
 int main() {
 
-    int num1, num2;
-    char op;
-    int result;
+    int index = 1;
 
-    cout << "Enter first number: ";
-    cin >> num1; 
-
-    cout << "Enter operator: ";
-    cin >> op; 
-
-    cout << "Enter second number: ";
-    cin >> num2;
-
-    switch (op)
-    {
-    case '+':
-        result = num1 + num2;
-        break;
-    
-    case '-':
-        result =  num1 - num2;
-        break;
-    
-    case '*':
-        result =  num1 * num2;
-        break;
-    
-    case '/':
-        result =  num1 / num2;
-        break;
-
-    default: 
-        cout << "Please exit program and enter a valid operator!" << endl;
+    while (index <= 5) {
+        cout << index << endl;
+        index++;
     }
-    
-
-    cout << result << endl;
 
     return 0;
 }
